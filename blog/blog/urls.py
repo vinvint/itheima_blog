35338cd322c16e8b4e332c524ec84890d7a15421
unknown <2212153235@qq.com>
2022-01-15 19:06:14 +0800

    注册界面展示

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-
-urlpatterns = [
+urlpatterns = (
     path('admin/', admin.site.urls),
-]
+    # include参数中有一个元组urlconf_module,app_name
+    path('', include(('users.urls', 'users'), namespace='users')),
+)
